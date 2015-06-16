9a4d18305fc2a782d98f98f9f4e50642d3fc55fa
Nazarett <gnazarett@gmail.com>
2015-06-16 00:02:17 -0400

    Last chance for gas variables

--- js/script.js
+++ js/script.js
@@ -3,9 +3,19 @@
 //Are you a heavy weight
 //Heavy enough to enter heavy weight division of the contest
 
-var heavyWeight = 250 + " lbs "
+var heavyWeight = 250
 
 //If weight is higher than 250lbs, you will enter the contest, other wise you are out.
 
-heavyWeight = (heavyWeight >= 250 + " lbs ") ? "You are a heavy weight" : "You will have to join another weight class"
+heavyWeight = (heavyWeight >= 250) ? "You are a heavy weight" : "You will have to join another weight class"
     console.log(heavyWeight)
+
+//Last chance for gas
+
+//Do you have to stop
+//Have enough gas to stop later
+
+var gasStation = 200 miles
+var gasEfficiency = 37 mpg
+var gasTank = 25 %
+var tankCapicity = 30 gallons
