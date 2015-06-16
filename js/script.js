46796924cb25556399f8da410e92c48810fbc911
Nazarett <gnazarett@gmail.com>
2015-06-15 23:40:44 -0400

    Name and input of heavy weight

---
+++ js/script.js
@@ -0,0 +1,11 @@
+//Gerardo D. Nazarett 06/15/15 Activity: Conditionals Worksheet
+
+//Are you a heavy weight
+//Heavy enough to enter heavy weight division of the contest
+
+var heavyWeight = 250 + " lbs "
+
+//If weight is higher than 250lbs, you will enter the contest, other wise you are out.
+
+heavyWeight = (heavyWeight >= 250 + " lbs ") ? "You are a heavy weight" : "You will have to join another weight class"
+    console.log(heavyWeight)
