8939be3944e1d14b4e0485c402e37fa02b4d3b34
Nazarett <gnazarett@gmail.com>
2015-06-16 23:53:08 -0400

    Add var and consoles

--- js/script.js
+++ js/script.js
@@ -3,19 +3,25 @@
 //Are you a heavy weight
 //Heavy enough to enter heavy weight division of the contest
 
-var heavyWeight = 250
+var heavyWeight = 250;
 
 //If weight is higher than 250lbs, you will enter the contest, other wise you are out.
 
-heavyWeight = (heavyWeight >= 250) ? "You are a heavy weight" : "You will have to join another weight class"
-    console.log(heavyWeight)
+heavyWeight = (heavyWeight >= 250) ? "You are a heavy weight" : "You will have to join another weight class";
+    console.log(heavyWeight);
 
 //Last chance for gas
 
 //Do you have to stop
 //Have enough gas to stop later
 
-var gasStation = 200 miles
-var gasEfficiency = 37 mpg
-var gasTank = 25 %
-var tankCapicity = 30 gallons
+var gasStationDist = 200; //Miles
+var gasEfficiency = 37; //Mpg
+var gasTankPercent = 25; //25% = .25
+var tankCapicity = 30; //Gallons 25% = 7.5 gallons
+
+console.log("The gas station is " + gasStationDist + " miles away.");
+console.log("My car does " + gasEfficiency + " mpg.");
+console.log("I have " + gasTankPercent + "% left on my tank.");
+console.log("The tank of my car can hold up to " + tankCapicity + " gallons");
+
